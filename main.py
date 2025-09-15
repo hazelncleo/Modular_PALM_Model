64055b8fbf7133f5b459e2d6e4a88ac822625640
Hazel <hazelncleo@gmail.com>
2025-09-15 16:31:06 +1000

    Updating Plan

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import json
-import glob
-from shutil import copy2 as copy_input_file
-from shutil import rmtree
-import inquirer
-from tkinter import Tk
-from tkinter.filedialog import askdirectory
-import string
 from Modular_Abaqus_Builder import Modular_Abaqus_Builder
 
 def main():
     
-
     # Instantiate class
     model = Modular_Abaqus_Builder()
 
