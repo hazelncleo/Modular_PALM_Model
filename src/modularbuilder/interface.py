a6d7b830dd81754500c82d4efcdf71dd45e5d1ac
hazel full <hazelncleo@gmail.com>
2026-04-13 21:23:25 +1000

    Very basic interface layout added.

--- src/modularbuilder/interface.py
+++ src/modularbuilder/interface.py
@@ -1,15 +1,50 @@
-from textual.app import App
-from textual.widgets import Welcome
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal, Vertical, Center
+from textual.widgets import Button, Header, Footer, Static, Tabs, Tab, ListView, ListItem, Label
 
 
-class WelcomeApp(App):
-    def on_key(self) -> None:
-        self.mount(Welcome())
+class ModularBuilderApp(App):
 
-    def on_button_pressed(self) -> None:
-        self.exit()
+    CSS_PATH='interface.tcss'
+
+    def compose(self) -> ComposeResult:
+        self.header = Header()
+        self.commandlist = ListView(
+            ListItem(Button('Object Commands')),
+            ListItem(Button('Model Commands')),
+            ListItem(Button('Database Commands')),
+            ListItem(Button('Settings')),
+            ListItem(Button('Quit'))
+        )
+        self.tabbedinfo = Tabs(
+            Tab('Database Tree'),
+            Tab('Database Info'),
+            Tab('File Structure'),
+            Tab('Log')
+        )
+        self.tabbedcontainer = Center(self.tabbedinfo)
+        self.footer = Footer()
+
+
+        yield self.header
+        yield Horizontal(
+            self.commandlist,
+            self.tabbedcontainer
+        )
+        yield self.footer
+
+    def on_mount(self) -> None:
+
+        self.commandlist.border_title = 'Commands'
+        self.tabbedcontainer.border_title = 'Info Tabs'
+
+    
+
+
+
+
 
 
 if __name__ == "__main__":
-    app = WelcomeApp()
+    app = ModularBuilderApp()
     app.run()
